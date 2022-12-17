10f26c3519505d2b8c325a8b84d1af9134cd2f35
ProphetLamb <prophet.lamb@gmail.com>
2022-12-17 18:29:18 +0100

    pyinstaller launchable

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+pyinstaller --onefile --windowed --icon='./img/logo.ico' --name='PoE High Spammer' --splash='./img/banner.png' --add-data='./img/logo.ico;img' --add-data='./img/banner.png;img/' launch.py
---
+++ launch.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import os
+import main
+import pyi_splash
+
+os.chdir(sys._MEIPASS)
+pyi_splash.close()
+main.main()
