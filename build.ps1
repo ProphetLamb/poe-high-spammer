53c332f3e021a4a787defd53a9012afee2ede337
ProphetLamb <prophet.lamb@gmail.com>
2022-12-17 18:31:56 +0100

    fix runsas bash

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/env pwsh
 # -*- coding: utf-8 -*-
 pyinstaller --onefile --windowed --icon='./img/logo.ico' --name='PoE High Spammer' --splash='./img/banner.png' --add-data='./img/logo.ico;img' --add-data='./img/banner.png;img/' launch.py
