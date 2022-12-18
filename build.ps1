71c8f552df2edf69505acd2b9beb8a0b3230829d
ProphetLamb <prophet.lamb@gmail.com>
2022-12-18 16:04:37 +0100

    fix assets path

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 #!/usr/bin/env pwsh
 # -*- coding: utf-8 -*-
 # single file for portable distribution
-pyinstaller --onefile --windowed --icon='./assets/logo.ico' --name='PoE High Spammer' --splash='./assets/banner.png' --add-data='./assets/*;assets/' --strip ./src/launch.py
+pyinstaller --onefile --windowed --icon='./assets/logo.ico' --name='PoE High Spammer' --splash='./assets/banner.png' --add-data='./assets/*;assets' --strip ./src/launch.py
 # multi file for innosetup distribution
-pyinstaller --onedir --windowed --icon='./assets/logo.ico' --name='PoE High Spammer' --splash='./assets/banner.png' --add-data='./assets/*;assets/' --strip ./src/launch.py
+pyinstaller --onedir --windowed --icon='./assets/logo.ico' --name='PoE High Spammer' --splash='./assets/banner.png' --add-data='./assets/*;assets' --strip ./src/launch.py
 iscc /Q .\setup.iss
